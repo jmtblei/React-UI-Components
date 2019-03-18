33dd4fb5a58c7d2eb4af12b6ab885c8f3997be4c
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-03-18 16:02:43 -0700

    react socialcard mvp and footer stretch done

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+                <i class="far fa-comment"></i>
+                <i class="fas fa-sync"></i> 6
+                <i class="far fa-heart"></i>4
+                <i class="far fa-envelope"></i>
+        </div>
+    )
+};
+
+export default Footer;
