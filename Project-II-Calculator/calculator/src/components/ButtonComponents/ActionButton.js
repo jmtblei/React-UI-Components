e9682a42cdb5d3523c18791f8f89cb9b21eebfe3
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-03-19 15:31:24 -0700

    calculator mvp finished, nonfunctional

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+function ActionButton(props) {
+    return (
+        <div className="actioncomponent">
+            <button className={props.style}>{props.text}</button>
+        </div>
+    )
+};
+
+export default ActionButton;
